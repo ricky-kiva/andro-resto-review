a1129f8d53ea204711ca557d26c2a130bd82b1e0
ricky-kiva <ricky.kiva@gmail.com>
2023-03-20 10:30:07 +0700

    add postReview()

--- app/src/main/java/com/rickyslash/restoreview/ApiService.kt
+++ app/src/main/java/com/rickyslash/restoreview/ApiService.kt
@@ -4,8 +4,18 @@ import retrofit2.http.*
 
 // 'interface' to request Api for link such 'https://restaurant-api.dicoding.dev/detail/{id}'
 interface ApiService {
+
     @GET("detail/{id}")
     fun getRestaurant(
         @Path("id") id: String
     ): Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
 }
